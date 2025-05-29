8c1556311197110259d57737a2a8a9f0b72732cb
trydev0303 <tpdbevf@gmail.com>
2025-05-28 22:35:27 -0400

    Change Edege function

--- netlify/edge-functions/blog-proxy.js
+++ netlify/edge-functions/blog-proxy.js
@@ -18,15 +18,15 @@ export default async (request, context) => {
 
     // Fix asset paths (CSS/JS/Images) in the response
     let html = await response.text();
-    html = html
-      .replace(
-        /(href|src)="\/(?!\/)/g, 
-        '$1="/blog'  // Rewrite root paths to /blog/
-      )
-      .replace(
-        /https:\/\/euphonious-torrone-5238d5\.netlify\.app\//g, 
-        '/blog'      // Replace full Astro blog URLs
-      );
+    // html = html
+    //   .replace(
+    //     /(href|src)="\/(?!\/)/g, 
+    //     '$1="/blog'  // Rewrite root paths to /blog/
+    //   )
+    //   .replace(
+    //     /https:\/\/euphonious-torrone-5238d5\.netlify\.app\//g, 
+    //     '/blog'      // Replace full Astro blog URLs
+    //   );
 
     // Return the modified response
     return new Response(html, {
